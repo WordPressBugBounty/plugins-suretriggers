9649c938be5322daa3ef0eeb20b06ad4052053af
Sean Murphy <sean@iamseanmurphy.com>
2025-03-18 08:18:12 +0000

    1.0.77

--- suretriggers/src/Integrations/late-point/actions/list-bundles.php
+++ suretriggers/src/Integrations/late-point/actions/list-bundles.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * AddNewRole.
+ * ListBundles.
  * php version 5.6
  *
- * @category AddNewRole
+ * @category ListBundles
  * @package  SureTriggers
  * @author   BSF <username@example.com>
  * @license  https://www.gnu.org/licenses/gpl-3.0.html GPLv3
@@ -11,51 +11,52 @@
  * @since    1.0.0
  */
 
-namespace SureTriggers\Integrations\Wordpress\Actions;
+namespace SureTriggers\Integrations\LatePoint\Actions;
 
+use Exception;
 use SureTriggers\Integrations\AutomateAction;
 use SureTriggers\Traits\SingletonLoader;
-use WP_User;
-use Exception;
 
 /**
- * AddNewRole
+ * ListBundles
  *
- * @category AddNewRole
+ * @category ListBundles
  * @package  SureTriggers
  * @author   BSF <username@example.com>
  * @license  https://www.gnu.org/licenses/gpl-3.0.html GPLv3
  * @link     https://www.brainstormforce.com/
  * @since    1.0.0
  */
-class AddNewRole extends AutomateAction {
+class ListBundles extends AutomateAction {
+
 
 	/**
 	 * Integration type.
 	 *
 	 * @var string
 	 */
-	public $integration = 'WordPress';
+	public $integration = 'LatePoint';
 
 	/**
 	 * Action name.
 	 *
 	 * @var string
 	 */
-	public $action = 'add_new_role';
+	public $action = 'lp_get_all_bundles';
 
 	use SingletonLoader;
 
 	/**
-	 * Register action.
+	 * Register a action.
 	 *
-	 * @param array $actions action data.
+	 * @param array $actions actions.
 	 * @return array
 	 */
 	public function register( $actions ) {
+
 		$actions[ $this->integration ][ $this->action ] = [
-			'label'    => __( 'Role: Add a new role to the user\'s roles', 'suretriggers' ),
-			'action'   => 'add_new_role',
+			'label'    => __( 'List Bundles', 'suretriggers' ),
+			'action'   => $this->action,
 			'function' => [ $this, 'action_listener' ],
 		];
 
@@ -68,22 +69,24 @@ class AddNewRole extends AutomateAction {
 	 * @param int   $user_id user_id.
 	 * @param int   $automation_id automation_id.
 	 * @param array $fields fields.
-	 * @param array $selected_options selectedOptions.
-	 * @return bool|array
+	 * @param array $selected_options selected_options.
+	 *
+	 * @return array|void
+	 *
 	 * @throws Exception Exception.
 	 */
 	public function _action_listener( $user_id, $automation_id, $fields, $selected_options ) {
-		$field = reset( $fields );
-		$user  = new WP_User( $user_id );
 
-		if ( ! ( $user instanceof WP_User ) ) {
-			throw new Exception( 'This user is not type of WP_User' );
+		global $wpdb;
+	
+		$bundles = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}latepoint_bundles", ARRAY_A );
+
+		if ( empty( $bundles ) ) {
+			return [ 'message' => 'No bundles found in the database.' ];
 		}
-		$user->add_role( $selected_options[ $field['name'] ] );
 
-		return (array) $user;
+		return $bundles;
 	}
-
 }
 
-AddNewRole::get_instance();
+ListBundles::get_instance();
